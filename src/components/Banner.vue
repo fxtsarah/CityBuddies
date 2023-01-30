3aaec93e25fa5f37e6cb61b67d0b957087ee3e82
Sarah K <fxtsarah@gmail.com>
2023-01-29 20:06:38 -0500

    adjust css

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,12 +2,14 @@
     <div id = "banner">
     <div id="banner_text">
       <h1>City Buddies</h1>
-      <p style="font-size: 20px;">See which city is nearest in population to your city</p>
+      <h5>See which city is nearest in population to your city</h5>
     </div>
 
     <div id="banner_form">
-      <input @keydown.enter="input_submit" placeholder="City name" class="form-control" :class="{ disabled_input: props.list_loading }" id="input_form" v-model="input_value" :tabindex=input_tab_index>
-      <button class="btn btn-light" :class="{ disabled_button: props.list_loading }" id="input_button" @keydown.enter="input_submit" @click="input_submit" :tabindex=submit_tab_index>Search For Buddy</button>
+      <div id="baner_input_and_button">
+        <input @keydown.enter="input_submit" placeholder="City name" class="form-control" :class="{ disabled_item: props.list_loading }" id="input_form" v-model="input_value" :tabindex=input_tab_index>
+        <button class="btn btn-light" :class="{ disabled_item: props.list_loading }" id="input_button" @keydown.enter="input_submit" @click="input_submit" :tabindex=submit_tab_index>Search For Buddy</button>
+      </div>
       <p v-if="props.list_loading" id="list_loading"><i>The cities list is loading, please wait...</i></p>
     </div>
   </div>
@@ -21,7 +23,6 @@ const emit = defineEmits(['input_submit'])
 const props = defineProps(['list_loading'])
 
 let input_value = ref("")
-let input = ref(null)
 
 const input_tab_index = computed(() => {
   return props.list_loading.value ? "-1" : "0"
