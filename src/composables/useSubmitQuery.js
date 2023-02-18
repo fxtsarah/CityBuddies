3a62957328f4a0df6f4e9a245eebc0447bbf4b64
Kolasa <fxtsarah@gmail.com>
2023-02-17 21:35:28 -0500

    https in main

--- src/composables/useSubmitQuery.js
+++ src/composables/useSubmitQuery.js
@@ -1,6 +1,9 @@
 // Import requirements to get a Sparql query.
 const axios = require('axios')
 const wbk = require('wikibase-sdk')
+const https = require('https')
+
+const httpsAgent = new https.Agent({ rejectUnauthorized: true });
 
 export function useSubmitQuery() {
     // Takes in SPARQL code as a parameter and sends the code to query.wikidata.org.
@@ -13,7 +16,7 @@ export function useSubmitQuery() {
 
         const url = wdk.sparqlQuery(sparql)
         try {
-            const results = await axios.get(url)
+            const results = await axios.get(url, { httpsAgent })
             let simplifiedResults = wbk.simplify.sparqlResults(results.data)
             return simplifiedResults
         }
