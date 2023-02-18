8d6651964e0a3cde5bca62eacc90fa689e3169be
Kolasa <fxtsarah@gmail.com>
2023-02-17 22:43:20 -0500

    experiment with reject unauthorized

--- backend/src/services/submitQueryService.js
+++ backend/src/services/submitQueryService.js
@@ -1,19 +1,9 @@
 // Import requirements to get a Sparql query.
 const axios = require('axios')
 const wbk = require('wikibase-sdk')
-const https = require('https')
-const fs = require('fs');
+// const https = require('https')
 
-const httpsAgent = new https.Agent({ rejectUnauthorized: false });
-
-// const httpsAgent = new https.Agent({ ca: fs.readFileSync(certPath) });
-
-// const httpsAgent = new https.Agent({
-//     rejectUnauthorized: false, // (NOTE: this will disable client verification)
-//     cert: fs.readFileSync("./usercert.pem"),
-//     key: fs.readFileSync("./key.pem"),
-//     passphrase: "YYY"
-//   })
+// const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 // Takes in SPARQL code as a parameter and sends the code to query.wikidata.org.
 // Simplifies the response from wikidata before returning it.
