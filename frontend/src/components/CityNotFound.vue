6d07d116d7218ec09d7fae5a14a22dd567f4e35d
Kolasa <fxtsarah@gmail.com>
2023-03-21 22:57:36 -0400

    add city search tip

--- frontend/src/components/CityNotFound.vue
+++ frontend/src/components/CityNotFound.vue
@@ -1,5 +1,6 @@
 <template>
     <h3 class='mt-3'>Sorry, we couldn't find a city with the name {{ route.params.targetLabel }}.</h3>
+    <h5>Make sure you only search for the name of the city (e.g. Berlin instead of Berlin, Germany)</h5>
 </template>
   
 <script setup>
