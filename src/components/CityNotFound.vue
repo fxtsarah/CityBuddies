6937558540c633062a4e31f04b2231674ccdab55
Kolasa <fxtsarah@gmail.com>
2023-03-21 23:06:08 -0400

    edit tip city not found

--- src/components/CityNotFound.vue
+++ src/components/CityNotFound.vue
@@ -1,6 +1,6 @@
 <template>
-    <h3 class='mt-3'>Sorry, we couldn't find a city with the name {{ route.params.targetLabel }}.</h3>
-    <h5>Make sure you only search for the name of the city (e.g. Berlin instead of Berlin, Germany)</h5>
+    <h3 class='mt-3 mx-3'>Sorry, we couldn't find a city with the name {{ route.params.targetLabel }}.</h3>
+    <h4 class="mx-3">Make sure you only search for the name of the city (e.g. Berlin instead of Berlin, Germany).</h4>
 </template>
   
 <script setup>
